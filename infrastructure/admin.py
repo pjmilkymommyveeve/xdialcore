d79d963456a478277cc30678ca43ee8d2e8cb26a
pjmommy <pjmilkymommyveeve@gmail.com>
2026-01-05 23:19:54 +0500

    added grouping for related extensions

--- infrastructure/admin.py
+++ infrastructure/admin.py
@@ -6,29 +6,29 @@ class ServerAdmin(admin.ModelAdmin):
     list_display = ['ip', 'alias', 'domain']
     search_fields = ['ip', 'alias', 'domain']
     list_filter = ['alias']
-    
+
     def has_module_permission(self, request):
         if not request.user.is_authenticated:
             return False
         return (request.user.is_superuser or 
                 request.user.is_admin or 
                 request.user.is_onboarding)
-    
+
     def has_view_permission(self, request, obj=None):
         if not request.user.is_authenticated:
             return False
         return request.user.is_superuser or request.user.is_admin or request.user.is_onboarding
-    
+
     def has_add_permission(self, request):
         if not request.user.is_authenticated:
             return False
         return request.user.is_superuser or request.user.is_admin or request.user.is_onboarding
-    
+
     def has_change_permission(self, request, obj=None):
         if not request.user.is_authenticated:
             return False
         return request.user.is_superuser or request.user.is_admin or request.user.is_onboarding
-    
+
     def has_delete_permission(self, request, obj=None):
         if not request.user.is_authenticated:
             return False
@@ -38,29 +38,34 @@ class ServerAdmin(admin.ModelAdmin):
 class ExtensionAdmin(admin.ModelAdmin):
     list_display = ['extension_number']
     search_fields = ['extension_number']
-    
+
+    def get_queryset(self, request):
+        """Order extensions by prefix (first two digits) to group them together"""
+        qs = super().get_queryset(request)
+        return qs.order_by('extension_number')
+
     def has_module_permission(self, request):
         if not request.user.is_authenticated:
             return False
         return (request.user.is_superuser or 
                 request.user.is_admin or 
                 request.user.is_onboarding)
-    
+
     def has_view_permission(self, request, obj=None):
         if not request.user.is_authenticated:
             return False
         return request.user.is_superuser or request.user.is_admin or request.user.is_onboarding
-    
+
     def has_add_permission(self, request):
         if not request.user.is_authenticated:
             return False
         return request.user.is_superuser or request.user.is_admin or request.user.is_onboarding
-    
+
     def has_change_permission(self, request, obj=None):
         if not request.user.is_authenticated:
             return False
         return request.user.is_superuser or request.user.is_admin or request.user.is_onboarding
-    
+
     def has_delete_permission(self, request, obj=None):
         if not request.user.is_authenticated:
             return False
